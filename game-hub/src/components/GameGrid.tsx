71f1f4a9d39e869aaade59a2652f997ced837eca
rohan-op <rsoh47@gmail.com>
2024-03-24 02:53:53 -0400

    Refactoring GameGrid Error

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -15,27 +15,26 @@ const GameGrid = ({ gameQuery }: Props) => {
   const skeleton = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20,
   ];
+
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={5}
-      >
-        {isLoading &&
-          skeleton.map((s) => (
-            // <GameCardContainer>
-            <GameCardSkeleton key={s}></GameCardSkeleton>
-            // </GameCardContainer>
-          ))}
-        {data.map((game) => (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      padding="10px"
+      spacing={5}
+    >
+      {isLoading &&
+        skeleton.map((s) => (
           // <GameCardContainer>
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCardSkeleton key={s}></GameCardSkeleton>
           // </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        // <GameCardContainer>
+        <GameCard key={game.id} game={game}></GameCard>
+        // </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
