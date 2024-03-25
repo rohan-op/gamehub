d59b1e9afc66b6e69cdc28980e706ca89493d9c6
rohan-op <rsoh47@gmail.com>
2024-03-25 17:49:42 -0400

    Hidding API key from code

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params:{
-        key: "fc4a1db4b8d04fcab83b402ecf6d9239"
+        key: import.meta.env.VITE_RAWG_API_KEY
     }
 });
