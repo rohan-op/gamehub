96ee7eddc114543167d295ea6bbcf49c85f0930b
rohan-op <rsoh47@gmail.com>
2024-03-30 13:13:04 -0400

    key test

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params:{
-        key: import.meta.env.VITE_RAWG_API_KEY
+        // key: import.meta.env.VITE_RAWG_API_KEY
+        key: "fc4a1db4b8d04fcab83b402ecf6d9239"
     }
 });
